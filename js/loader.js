066bbdda97747a1a55cfd415a5def238fb9571ab
Ian Agustin <inbagustin@gmail.com>
2020-01-07 16:08:30 +0800

    Disposes loader to save memory

--- js/loader.js
+++ js/loader.js
@@ -3,4 +3,7 @@ let loader = document.getElementById("loader-bg");
 window.addEventListener('load', setTimeout(function() {
     loader.style.opacity = 0;
     loader.style.zIndex = -1000;
+    setTimeout(function() {
+        loader.style.display = "none";
+    }, 1000)
 }, 250));
