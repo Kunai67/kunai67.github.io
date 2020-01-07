138ce72872a371d2785b53bf01681ec605fb486a
Ian Agustin <inbagustin@gmail.com>
2020-01-07 15:59:47 +0800

    Updated Loader Transition

--- js/loader.js
+++ js/loader.js
@@ -1,5 +1,6 @@
 let loader = document.getElementById("loader-bg");
 
-window.addEventListener('load', function() {
-    loader.style.display = "none";
-});
+window.addEventListener('load', setTimeout(function() {
+    loader.style.opacity = 0;
+    loader.style.zIndex = -1000;
+}, 750));
