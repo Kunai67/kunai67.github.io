988e690a500a8d0530045e160ba64920c50e2666
Ian Agustin <inbagustin@gmail.com>
2020-01-07 16:04:54 +0800

    Faster Transitions for Loader

--- js/loader.js
+++ js/loader.js
@@ -3,4 +3,4 @@ let loader = document.getElementById("loader-bg");
 window.addEventListener('load', setTimeout(function() {
     loader.style.opacity = 0;
     loader.style.zIndex = -1000;
-}, 750));
+}, 250));
